d182b5d3d6c84434099c9d0af7c07a22cf1d99be
sonledinh <sonledinh@admicro.vn>
2021-03-26 01:13:16 +0700

    update index

--- form-pit.php
+++ form-pit.php
@@ -10,11 +10,41 @@
 		 			<div class="item-form">
 		 				<div class="title-form">Personal information</div>
 		 				<ul>
-		 					<li><input type="text" placeholder=""></li>
-		 					<li><input type="text" placeholder=""></li>
-		 					<li><input type="text" placeholder=""></li>
-		 					<li><input type="text" placeholder=""></li>
+		 					<li><input type="text" placeholder="Your name"></li>
+		 					<li><input type="text" placeholder="Year of Birth"></li>
+		 					<li><input type="text" placeholder="Your school / work place"></li>
+		 					<li><input type="text" placeholder="Design course has participated"></li>
 		 				</ul>
+		 			</div>
+		 			<div class="item-form">
+		 				<div class="title-form">Contact information</div>
+		 				<ul>
+		 					<li><input type="text" placeholder="Your email"></li>
+		 					<li><input type="text" placeholder="Your phone number"></li>
+		 				</ul>
+		 			</div>
+		 			<div class="item-form">
+		 				<div class="title-form">
+		 					Your creation <span>Summarize any of your best design experiences here</span>
+		 				</div>
+		 				<ul>
+		 					<li><input type="text" placeholder="Portfolio Link"></li>
+		 					<li><input type="text" placeholder="Or Behance link"></li>
+		 					<li><input type="text" placeholder="Or Dribbble link"></li>
+		 				</ul>
+		 			</div>
+		 			<div class="item-form">
+		 				<div class="title-form">Do you have a message to Pit?</div>
+		 				<ul>
+		 					<li>
+		 						<textarea name="" id="" cols="30" rows="10" placeholder="We are ready to listen to any questions you may have ..."></textarea>
+		 					</li>
+		 				</ul>
+		 			</div>
+		 			<div class="item-form">
+		 				<div class="btn-form">
+		 					<input type="submit" value="Registration">
+		 				</div>
 		 			</div>
 		 		</div>
 	 		</div>
