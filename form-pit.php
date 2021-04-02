b068ac257cb70f3102ea0781d4922fffd534a68c
minhpn <minhpn@tinhvan.com>
2021-04-02 23:38:38 +0700

    form

--- form-pit.php
+++ form-pit.php
@@ -1,4 +1,27 @@
 <?php include 'header.php';?>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v10.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="2221969247888734">
+      </div>
 <main>
 	 <section class="form-studio">
 	 	<div class="container">
